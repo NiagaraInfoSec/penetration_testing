f0373309175f2192b454e4f1f05fb896d6d896f3
Ella Rose <python_pride@protonmail.com>
2018-02-17 17:02:00 -0500

    fixed readme, started interceptor

---
+++ interceptor/interceptor.py
@@ -0,0 +1,62 @@
+import StringIO
+import email
+import socket
+
+import pride.components.network
+
+
+class Connecting_Socket(pride.components.network.Tcp_Client):
+    
+    defaults = {"request_data" : ''}
+    required_attributes = ("request_data", )
+    
+    def on_connect(self):
+        super(Connecting_Socket, self).on_connect()
+        self.send(self.request_data)
+        #self.alert("Sent:\n{}".format(self.request_data))
+        self.alert("Connected as {}".format(self.sockname))
+        
+    def recv(self):
+        data = super(Connecting_Socket, self).recv()
+        #self.alert("Received:\n{}".format(data))
+        self.delete()
+        
+        
+class Interceptor_Proxy_Socket(pride.components.network.Tcp_Socket):
+    
+    defaults = {"outgoing_connection" : Connecting_Socket}
+    
+    def recv(self):
+        raw_data = super(Interceptor_Proxy_Socket, self).recv()
+        request_line, header_data = raw_data.split('\r\n', 1)
+        _stringio = StringIO.StringIO(header_data)
+        headers = email.message_from_file(_stringio)
+        try:
+            host, port = headers["Host"].split(':', 1)
+        except ValueError:
+            print "ValueError", headers["Host"]
+            host = headers["Host"]
+            port = 80
+        ip = socket.gethostbyname(host)        
+        assert ip != "127.0.0.1"
+        assert ip != "localhost"
+        connection = self.create(self.outgoing_connection, ip=ip, port=int(port), request_data=raw_data)
+                
+    
+class Interceptor_Proxy_Server(pride.components.network.Server):
+    
+    defaults = {"port" : 8080, "Tcp_Socket_type" : Interceptor_Proxy_Socket}
+    
+    def delete(self):
+        self.alert("Deleting")
+        super(Interceptor_Proxy_Server, self).delete()
+        
+        
+def unit_test():
+    print "Creating server..."
+    server = pride.objects["/Python"].create(Interceptor_Proxy_Server)
+    server.alert("Server created")
+    
+if __name__ == "__main__":
+    unit_test()
+    
--- readme.md
+++ readme.md
@@ -3,11 +3,13 @@ Ellas Penetration Testing Repo
 This repository contains code and data related to web application penetration testing
 
 Layout:
-    - data: Contains useful data files
-        - commonsurnames.txt: A list of common last names, useful for answering security questions such as "What is your mother's maiden name"
-    - findings: Lists of issues found with vulnerable web applications (Spoiler warning)
-    - misc: Miscellaneous files that may/may not be very useful
-    - sqlmap: The SQLMAP python tool (not authored by myself)
-    - utilities: useful code
+
+- data: Contains useful data files
+    - commonsurnames.txt: A list of common last names, useful for answering security questions such as "What is your mother's maiden name"
+- findings: Lists of issues found with vulnerable web applications (Spoiler warning)
+- interceptor: The start of an interceptor proxy written in python/pride
+- misc: Miscellaneous files that may/may not be very useful
+- sqlmap: The SQLMAP python tool (not authored by myself)
+- utilities: useful code
     
     
