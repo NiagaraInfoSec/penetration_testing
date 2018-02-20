236f97c9447b99c0ccf52a1d7103eab22b3cb38b
Ella Rose <python_pride@protonmail.com>
2018-02-20 16:16:11 -0500

    fixed street names ordering

--- utilities/extractstreets.py
+++ utilities/extractstreets.py
@@ -9,9 +9,14 @@ def extract_streets():
         with open(filename, "r") as input_file:
             destination_filename = os.path.join(source_dir, os.path.split(filename)[-1])
             with open(destination_filename, 'w') as destination_file:
-                streets = reversed(sorted(input_file.readlines()))
-                destination_file.write(''.join(item.split('\t')[1] for item in streets))
-                                            
+                street_list = []
+                for street in input_file.readlines():
+                    frequency, street = street.split('\t', 1)
+                    street_list.append((int(frequency), street))
+                                
+                for frequency, street in reversed(sorted(street_list)):                              
+                    destination_file.write(street)
+                                                            
 if __name__ == "__main__":
     extract_streets()
         
